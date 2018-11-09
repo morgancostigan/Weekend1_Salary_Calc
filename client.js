91412e359a7502b84debe6ba5bb1569b4a6c87fb
Morgan Costigan <morgan.costigan@gmail.com>
2018-11-09 15:15:21 -0600

    html framework started

---
+++ client.js
@@ -0,0 +1,12 @@
+console.log('JS');
+
+$(document).ready( jQReady );
+
+function addClickHandlers() {
+    
+}// end addClickHandlers function
+
+function jQReady() {
+    console.log('$ up'); 
+    addClickHandlers();
+}// end jQReady function
