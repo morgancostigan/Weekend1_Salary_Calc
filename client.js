57d579f01510d37a6be27e797a697e36c4ca05fe
Morgan Costigan <morgan.costigan@gmail.com>
2018-11-09 15:36:01 -0600

    set table head, inputs working and self clearing

--- client.js
+++ client.js
@@ -2,9 +2,44 @@ console.log('JS');
 
 $(document).ready( jQReady );
 
+let employeeArray = [];
+
+class Employee {
+    constructor(firstNameIn, lastNameIn, employeeIDIn, titleIn, annualSalaryIn) {
+        this.firstName = firstNameIn;
+        this.lastName = lastNameIn;
+        this.employeeID = employeeIDIn;
+        this.title = titleIn;
+        this.annualSalary = annualSalaryIn;
+    }
+}
+
 function addClickHandlers() {
-    
+    console.log('in addClickHandlers');
+    $('#inputSubmitButton').on('click', inputSubmit);
 }// end addClickHandlers function
+
+function appendDom() {
+    console.log('in appendDom');
+}//end appendDom function
+
+function inputSubmit() {
+    console.log('in inputSubmit');
+    let firstName = $('#firstNameInput').val();
+    $('#firstNameInput').val('');
+    let lastName = $('#lastNameInput').val();
+    $('#lastNameInput').val('');
+    let employeeID = $('#employeeIDInput').val();
+    $('#employeeIDInput').val('');
+    let title = $('#titleInput').val();
+    $('#titleInput').val('');
+    let annualSalary = $('#annualSalaryInput').val();
+    $('#annualSalaryInput').val('');
+    console.log(firstName, lastName, employeeID, title, annualSalary);
+    let employee = new Employee(firstName, lastName, employeeID, title, annualSalary);
+    employeeArray.push(employee);
+    appendDom();
+}// end inputSubmit function
 
 function jQReady() {
     console.log('$ up'); 
