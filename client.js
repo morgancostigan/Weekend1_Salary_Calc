257d4ff4d060846d05512f5be6595660227a7260
Morgan Costigan <morgan.costigan@gmail.com>
2018-11-10 18:35:39 -0600

    delete buttons, deleting but not running new math yet, over 20k red

--- client.js
+++ client.js
@@ -3,6 +3,7 @@ console.log('JS');
 $(document).ready( jQReady );
 
 let employeeArray = [];
+let totalAnnualSalary = [];
 
 class Employee {
     constructor(firstNameIn, lastNameIn, employeeIDIn, titleIn, annualSalaryIn) {
@@ -23,15 +24,45 @@ function appendDom() {
     console.log('in appendDom');
     $('#tableBody').empty();
     for (let index = 0; index < employeeArray.length; index++) {
-        let firstName = employeeArray[index].firstName;
-        let lastName = employeeArray[index].lastName;
-        let employeeID = employeeArray[index].employeeID;
-        let title = employeeArray[index].title;
-        let annualSalary = employeeArray[index].annualSalary;
-        let tablerow = $(`<tr><th>${firstName}</th><th>${lastName}</th><th>${employeeID}</th><th>${title}</th><th>${annualSalary}</th>`);
+        const emp = employeeArray[index];
+        let tablerow = $(`<tr class="${emp.employeeID}">
+        <td>${emp.firstName}</td>
+        <td>${emp.lastName}</td>
+        <td>${emp.employeeID}</td>
+        <td>${emp.title}</td>
+        <td>${emp.annualSalary}</td>
+        <td><button class="${emp.employeeID}">DELETE</button>
+        </td></tr>`);
         $('#tableBody').append(tablerow);
-    }// end for loop
+        $(`.${emp.employeeID}`).on('click', function () {
+            console.log('baleeted');
+            $(`.${emp.employeeID}`).remove();
+            $('#totalMonthlyPay').val('');
+            
+
+
+        }//end ANON function
+        )//end on click
+    }//end for loop
 }//end appendDom function
+
+// function deleteEmployee() {
+//     console.log('in delete button');
+// }//end deleteEmployee
+
+function divideMonthly(numToDivide) {
+    // console.log('in divideMonthly');
+    let totalMonthlyPay = numToDivide / 12;
+    console.log('total monthly:' + totalMonthlyPay.toFixed(2));
+    $('#totalMonthlyPay').empty();
+    if (totalMonthlyPay.toFixed(2) <= 20000) {
+        $('#totalMonthlyPay').append('Total Monthly: ', totalMonthlyPay.toFixed(2));
+    }//end <20
+    else{
+        $('#totalMonthlyPay').css('background-color', 'red');
+        $('#totalMonthlyPay').append('Total Monthly: ', totalMonthlyPay.toFixed(2));
+    }
+}//end divideMonthly function
 
 function inputSubmit() {
     console.log('in inputSubmit');
@@ -39,19 +70,29 @@ function inputSubmit() {
     $('#firstNameInput').val('');
     let lastName = $('#lastNameInput').val();
     $('#lastNameInput').val('');
-    let employeeID = $('#employeeIDInput').val();
+    let employeeID = parseInt($('#employeeIDInput').val());
     $('#employeeIDInput').val('');
     let title = $('#titleInput').val();
     $('#titleInput').val('');
-    let annualSalary = $('#annualSalaryInput').val();
+    let annualSalary = parseInt($('#annualSalaryInput').val());
     $('#annualSalaryInput').val('');
     console.log(firstName, lastName, employeeID, title, annualSalary);
     let employee = new Employee(firstName, lastName, employeeID, title, annualSalary);
     employeeArray.push(employee);
     appendDom();
+    sumUpSalary();
 }// end inputSubmit function
 
 function jQReady() {
     console.log('$ up'); 
     addClickHandlers();
 }// end jQReady function
+
+function sumUpSalary() {
+    // console.log('in sumUpSalary'); 
+    let totalPay = 0;
+    for (let index = 0; index < employeeArray.length; index++) {
+         totalPay += employeeArray[index].annualSalary;
+    }
+    divideMonthly(totalPay);
+}//end sumUpSalary function
